828a4bbd3d3f572870e96528711ddb8891a2f217
carafie <252391798+carafie@users.noreply.github.com>
2026-07-27 19:12:42 +0200

    feat: add KindUnknown variant and NewKind function

--- platform/jwt/kind.go
+++ platform/jwt/kind.go
@@ -1,8 +1,24 @@
 package jwt
 
+import "errors"
+
+var ErrKindInvalid = errors.New("kind is invalid")
+
 type Kind int
 
 const (
-	KindAccess Kind = iota
+	kindUnknown Kind = iota
+	KindAccess
 	KindRefresh
 )
+
+func NewKind(kind int) (Kind, error) {
+	switch kind {
+	case int(KindAccess):
+		return KindAccess, nil
+	case int(KindRefresh):
+		return KindRefresh, nil
+	default:
+		return kindUnknown, ErrKindInvalid
+	}
+}
