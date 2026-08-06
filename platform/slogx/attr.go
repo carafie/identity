ed11fa0154eb59673c35043a694fcc1679c9333f
carafie <252391798+carafie@users.noreply.github.com>
2026-08-06 21:46:35 +0200

    feat: add slogx package
    The package extends standard library
    with slog attributes for consistent log keys.

---
+++ platform/slogx/attr.go
@@ -0,0 +1,19 @@
+package slogx
+
+import "log/slog"
+
+const (
+	errorKey     = "error"
+	requestIDKey = "request_id"
+)
+
+func Error(err error) slog.Attr {
+	if err != nil {
+		return slog.String(errorKey, err.Error())
+	}
+	return slog.Attr{}
+}
+
+func RequestID(id string) slog.Attr {
+	return slog.String(requestIDKey, id)
+}
