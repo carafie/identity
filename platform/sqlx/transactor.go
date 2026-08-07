b29582ae05401150eea4e67cb33137e234f2d197
carafie <252391798+carafie@users.noreply.github.com>
2026-08-07 23:18:07 +0200

    refactor: add TCLs for atomic transactions

--- platform/sqlx/transactor.go
+++ platform/sqlx/transactor.go
@@ -5,8 +5,17 @@ import (
 )
 
 type Transactor interface {
-	Single(ctx context.Context, work TransactorWork) error
-	Atomic(ctx context.Context, work TransactorWork) error
+	Single(ctx context.Context, work TransactorSingleWork) error
+	Atomic(ctx context.Context, work TransactorAtomicWork) error
 }
 
-type TransactorWork func(ctx context.Context, executor Executor) error
+type TransactorSingleWork func(ctx context.Context, executor Executor) error
+
+type TransactorAtomicWork func(ctx context.Context, executor Executor) (TCL, error)
+
+type TCL int
+
+const (
+	Commit TCL = iota + 1
+	Rollback
+)
